3c2721bd47dd0ae68f075fc5ddd2df1c733190de
Pavs315 <boddu.pavani@research.iiit.ac.in>
2021-10-03 01:58:21 +0530

    DFT function checked

--- DFT/DFT.m
+++ DFT/DFT.m
@@ -5,7 +5,7 @@ N = length(x);
 X = zeros(N,1);
 for k = 0:N-1
     for n = 0:N-1
-        X(k+1) = X(k+1) + x(n+1)*exp(-1j*pi/2*n*k);   %generating DFT
+        X(k+1) = X(k+1) + x(n+1)*exp(-1j*2*pi*n*k/N);   %generating DFT
     end
 end
 end
