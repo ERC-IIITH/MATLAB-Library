f1a113ee65b0d34a2b1859a7e14409c328646685
Pavs315 <boddu.pavani@research.iiit.ac.in>
2021-10-03 01:42:33 +0530

    DFT Function and script

--- DFT/DFT.m
+++ DFT/DFT.m
@@ -1,30 +1,5 @@
 %Function to perform discrete fourier transform of an input sequence x[n] without using inbuilt functions
 
-clc;
-x1 = [1 2 3 4];
-X1 = DFT(x1);
-N = length(x1);
-
-%plotting the input sequence and the DFT
-t = 0:N-1;
-subplot(311)
-stem(t,x);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Input sequence')
-subplot(312)
-stem(t,abs(X))
-xlabel('Frequency');
-ylabel('|X(k)|');
-title('Magnitude response')
-%plotting the phase response in frequency domain
-subplot(313)
-stem(t,angle(X))
-xlabel('Frequency');
-ylabel('Phase');
-title('Phase response')
-
-%the dft function 
 function X = DFT(x) 
 N = length(x);
 X = zeros(N,1);
