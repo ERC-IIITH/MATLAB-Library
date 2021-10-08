c663b2a6721fd0904e30b1dd32fe660f4ec7d9b8
Pranav Manu <71229090+raipranav384@users.noreply.github.com>
2021-10-08 09:27:43 +0530

    Vector A can now be column or row vector

--- PartialFourierSum/partialFourierSum.m
+++ PartialFourierSum/partialFourierSum.m
@@ -3,7 +3,7 @@ function y = partialFourierSum (A, T,time_grid)
 % T is the Time period of the given signal
 % Time grid is the region and precision specified for time axis
 
-sz=size(A,2);
+sz=length(A);
 if mod(sz,2)==0
    error('Error.\n Ivalind length of vector of Fourier coefficients :%d',sz);
 end
@@ -11,6 +11,6 @@ end
 N=(sz-1)/2;
 y = zeros(size(time_grid));
     for k=-N:N
-        y = y + A(1,N+1+k)*exp((1i)*k*(2*(pi)/T)*time_grid);
+        y = y + A(N+1+k)*exp((1i)*k*(2*(pi)/T)*time_grid);
     end
 end
